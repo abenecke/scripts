6bba997047d25bdd8999b29cd62b491be3ff43a2
Anna Benecke <anna.benecke@desy.de>
2017-04-12 11:39:22 +0200

    add plot for lowest mass point

--- plots/npv_eff.cxx
+++ plots/npv_eff.cxx
@@ -18,7 +18,9 @@ void npv_eff()
   gROOT->SetBatch(kFALSE);
 
   TFile *Data_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.Data.Data.root", "READ");
-  TFile *Signal_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.MC_ZPrime20001200HT.root", "READ");
+  //  TFile *Signal_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.MC_ZPrime20001200HT.root", "READ");
+  TFile *Signal_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.MC_ZPrime1500ToTPrime700T_TPrimeToHT.root", "READ");
+  TFile *Bkg_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.background.root", "READ");
 
   TH1F *nvp_nocuts_data = (TH1F*)Data_f ->Get("input/N_PrimVertices");
   TH1F *nvp_preselection_data = (TH1F*)Data_f ->Get("event_twodcut/N_PrimVertices");
@@ -28,17 +30,27 @@ void npv_eff()
   TH1F *nvp_preselection_signal = (TH1F*)Signal_f ->Get("event_twodcut/N_PrimVertices");
   TH1F *nvp_selection_signal = (TH1F*)Signal_f ->Get("event_chi2cut/N_PrimVertices");
 
-  // TGraphAsymmErrors* eff_presel_data = new TGraphAsymmErrors( nvp_preselection_data, nvp_nocuts_data, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_sel_data = new TGraphAsymmErrors( nvp_selection_data, nvp_nocuts_data, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_tagged_data = new TGraphAsymmErrors( nvp_selection_data, nvp_preselection_data, "cl=0.683 b(1,1) mode" );
+  TH1F *nvp_nocuts_bkg = (TH1F*)Bkg_f ->Get("input/N_PrimVertices");
+  TH1F *nvp_preselection_bkg = (TH1F*)Bkg_f ->Get("event_twodcut/N_PrimVertices");
+  TH1F *nvp_selection_bkg = (TH1F*)Bkg_f ->Get("event_chi2cut/N_PrimVertices");
 
-  // TGraphAsymmErrors* eff_presel_signal = new TGraphAsymmErrors( nvp_preselection_signal, nvp_nocuts_signal, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_sel_signal = new TGraphAsymmErrors( nvp_selection_signal, nvp_nocuts_signal, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_tagged_signal = new TGraphAsymmErrors( nvp_selection_signal, nvp_preselection_signal, "cl=0.683 b(1,1) mode" );
+
+  TGraphAsymmErrors* eff_presel_data = new TGraphAsymmErrors( nvp_preselection_data, nvp_nocuts_data, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_data = new TGraphAsymmErrors( nvp_selection_data, nvp_nocuts_data, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_tagged_data = new TGraphAsymmErrors( nvp_selection_data, nvp_preselection_data, "cl=0.683 b(1,1) mode" );
+
+  TGraphAsymmErrors* eff_presel_signal = new TGraphAsymmErrors( nvp_preselection_signal, nvp_nocuts_signal, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_signal = new TGraphAsymmErrors( nvp_selection_signal, nvp_nocuts_signal, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_tagged_signal = new TGraphAsymmErrors( nvp_selection_signal, nvp_preselection_signal, "cl=0.683 b(1,1) mode" );
+
+  TGraphAsymmErrors* eff_presel_bkg = new TGraphAsymmErrors( nvp_preselection_bkg, nvp_nocuts_bkg, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_bkg = new TGraphAsymmErrors( nvp_selection_bkg, nvp_nocuts_bkg, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_tagged_bkg = new TGraphAsymmErrors( nvp_selection_bkg, nvp_preselection_bkg, "cl=0.683 b(1,1) mode" );
 
   folder="chsvspuppi";
   TFile *Data_puppi_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.Data.Data.root", "READ");
   TFile *Signal_puppi_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.MC_ZPrime20001200HT.root", "READ");
+  TFile *Bkg_puppi_f = new TFile(directory+folder+"/uhh2.AnalysisModuleRunner.MC.background.root", "READ");
 
   TH1F *nvp_nocuts_data_puppi = (TH1F*)Data_puppi_f ->Get("input/N_PrimVertices");
   TH1F *nvp_preselection_data_puppi = (TH1F*)Data_puppi_f ->Get("event_twodcut/N_PrimVertices");
@@ -47,30 +59,55 @@ void npv_eff()
   TH1F *nvp_nocuts_signal_puppi = (TH1F*)Signal_puppi_f ->Get("input/N_PrimVertices");
   TH1F *nvp_preselection_signal_puppi = (TH1F*)Signal_puppi_f ->Get("event_twodcut/N_PrimVertices");
   TH1F *nvp_selection_signal_puppi = (TH1F*)Signal_puppi_f ->Get("event_chi2cut/N_PrimVertices");
-  nvp_preselection_signal_puppi->Rebin(2);
-  nvp_selection_signal_puppi->Rebin(2);
-  nvp_nocuts_signal_puppi->Rebin(2);
-  // TGraphAsymmErrors* eff_presel_data_puppi = new TGraphAsymmErrors( nvp_preselection_data_puppi, nvp_nocuts_data_puppi, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_sel_data_puppi = new TGraphAsymmErrors( nvp_selection_data_puppi, nvp_nocuts_data_puppi, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_tagged_data_puppi = new TGraphAsymmErrors( nvp_selection_data_puppi, nvp_preselection_data_puppi, "cl=0.683 b(1,1) mode" );
 
-  // TGraphAsymmErrors* eff_presel_signal_puppi = new TGraphAsymmErrors( nvp_preselection_signal_puppi, nvp_nocuts_signal_puppi, "cl=0.683 b(1,1) mode" );
-  // TGraphAsymmErrors* eff_sel_signal_puppi = new TGraphAsymmErrors( nvp_selection_signal_puppi, nvp_nocuts_signal_puppi, "cl=0.683 b(1,1) mode" );
+  TH1F *nvp_nocuts_bkg_puppi = (TH1F*)Bkg_puppi_f ->Get("input/N_PrimVertices");
+  TH1F *nvp_preselection_bkg_puppi = (TH1F*)Bkg_puppi_f ->Get("event_twodcut/N_PrimVertices");
+  TH1F *nvp_selection_bkg_puppi = (TH1F*)Bkg_puppi_f ->Get("event_chi2cut/N_PrimVertices");
+
+  // TH1F *hist = nvp_nocuts_signal_puppi;
+  // for (int i=1; i<hist->GetNbinsX()+1; ++i){
+  //   cout << hist->GetXaxis()->GetBinLowEdge(i) << ", ";}
+  Double_t bins[55]={0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5, 22.5, 23.5, 24.5, 25.5, 26.5, 27.5, 28.5, 29.5, 30.5, 31.5, 32.5, 33.5, 34.5, 35.5, 36.5, 37.5, 38.5, 39.5, 40.5, 41.5, 42.5, 43.5, 44.5, 45.5, 46.5, 47.5, 48.5, 49.5, 50.5, 51.5, 52.5, 53.5, 54.5};
+  nvp_preselection_signal_puppi->Rebin(54, "nvp_preselection_signal_puppi",bins);
+  nvp_selection_signal_puppi->Rebin(54, "nvp_selection_signal_puppi",bins);
+  nvp_nocuts_signal_puppi->Rebin(54, "nvp_nocuts_signal_puppi",bins);
+  TGraphAsymmErrors* eff_presel_data_puppi = new TGraphAsymmErrors( nvp_preselection_data_puppi, nvp_nocuts_data_puppi, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_data_puppi = new TGraphAsymmErrors( nvp_selection_data_puppi, nvp_nocuts_data_puppi, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_tagged_data_puppi = new TGraphAsymmErrors( nvp_selection_data_puppi, nvp_preselection_data_puppi, "cl=0.683 b(1,1) mode" );
+
+  TGraphAsymmErrors* eff_presel_signal_puppi = new TGraphAsymmErrors( nvp_preselection_signal_puppi, nvp_nocuts_signal_puppi, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_signal_puppi = new TGraphAsymmErrors( nvp_selection_signal_puppi, nvp_nocuts_signal_puppi, "cl=0.683 b(1,1) mode" );
   TGraphAsymmErrors* eff_tagged_signal_puppi = new TGraphAsymmErrors( nvp_selection_signal_puppi, nvp_preselection_signal_puppi, "cl=0.683 b(1,1) mode" );
-  nvp_selection_signal_puppi->Draw("AP");
-  nvp_preselection_signal_puppi->SetLineColor(kRed);
-  nvp_preselection_signal_puppi->Draw("same");
-  // std::vector<TGraphAsymmErrors*> hists={eff_presel_data,eff_presel_signal,eff_presel_data_puppi,eff_presel_signal_puppi};
-  // plot_result( hists,  1, 0.001);
+
+  TGraphAsymmErrors* eff_presel_bkg_puppi = new TGraphAsymmErrors( nvp_preselection_bkg_puppi, nvp_nocuts_bkg_puppi, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_sel_bkg_puppi = new TGraphAsymmErrors( nvp_selection_bkg_puppi, nvp_nocuts_bkg_puppi, "cl=0.683 b(1,1) mode" );
+  TGraphAsymmErrors* eff_tagged_bkg_puppi = new TGraphAsymmErrors( nvp_selection_bkg_puppi, nvp_preselection_bkg_puppi, "cl=0.683 b(1,1) mode" );
+  // nvp_selection_signal_puppi->Draw("AP");
+  // nvp_preselection_signal_puppi->SetLineColor(kRed);
+  // nvp_preselection_signal_puppi->Draw("same");
+  std::vector<TGraphAsymmErrors*> hists={eff_presel_data,eff_presel_signal,eff_presel_data_puppi,eff_presel_signal_puppi,eff_presel_bkg,eff_presel_bkg_puppi};
+  plot_result( hists,  1, 0.001);
 
 
-  // hists={eff_sel_data,eff_sel_signal,eff_sel_data_puppi,eff_sel_signal_puppi};
-  // plot_result( hists,  1, 0.0001);
+  hists={eff_sel_data,eff_sel_signal,eff_sel_data_puppi,eff_sel_signal_puppi,eff_sel_bkg,eff_sel_bkg_puppi};
+  plot_result( hists,  1, 0.0001);
 
  
-  // hists={eff_tagged_data,eff_tagged_signal,eff_tagged_data_puppi,eff_tagged_signal_puppi};
-  // plot_result( hists,  1, 0.00000001);
+  hists={eff_tagged_data,eff_tagged_signal,eff_tagged_data_puppi,eff_tagged_signal_puppi,eff_tagged_bkg,eff_tagged_bkg_puppi};
+  plot_result( hists,  1, 0.00000001);
 
+
+
+  hists={eff_presel_bkg,eff_presel_signal,eff_presel_bkg_puppi,eff_presel_signal_puppi};
+  plot_result( hists,  1, 0.001);
+
+
+  hists={eff_sel_bkg,eff_sel_signal,eff_sel_bkg_puppi,eff_sel_signal_puppi};
+  plot_result( hists,  1, 0.0001);
+
+ 
+  hists={eff_tagged_bkg,eff_tagged_signal,eff_tagged_bkg_puppi,eff_tagged_signal_puppi};
+  plot_result( hists,  1, 0.00000001);
 
 
 }//end of void function
